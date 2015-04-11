b3113a4510e5e3a7c2de8d81837a9d659be24119
Karan Sharma <karan1276@gmail.com>
2015-04-11 15:58:45 +0530

    exported all files in this new directory

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$('.rotate1').textRotator({
+            // These are the defaults.
+            animation: "fadeIn",
+            seprator: ",",
+            speed: "2000"
+        });
+    $('.rotate2').textRotator({
+            // These are the defaults.
+            animation: "rotateIn",
+            seprator: ",",
+            speed: "2000"
+        });
+    $('.rotate3').textRotator({
+            // These are the defaults.
+            animation: "zoomIn",
+            seprator: ",",
+            speed: "2000"
+        });	
+});
